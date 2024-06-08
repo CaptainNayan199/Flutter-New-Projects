4a8994544a8ed3e40980ae29c174c63f2d73a635
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-08 21:57:43 +0545

    Creating home page UI

--- todo_app/lib/pages/home.dart
+++ todo_app/lib/pages/home.dart
@@ -10,11 +10,12 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool today=true, tomorrow = false, next_week = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(top: 80.0, left: 50.0),
+        padding: EdgeInsets.only(top: 50.0, left: 50.0),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         decoration: BoxDecoration(
@@ -22,18 +23,21 @@ class _HomeState extends State<Home> {
             Color(0xFF4E65FF),
             Color(0xFF92EFFD),
             Color.fromARGB(255, 34, 236, 118)
-          ], begin: Alignment.topRight, end: Alignment.bottomLeft),
+          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              "Hello\nNayan",
+              "HELLO\nUJJWAL",
               style: TextStyle(
-                fontSize: 30.0,
+                fontSize: 35.0,
                 color: Colors.white,
                 fontWeight: FontWeight.w600,
               ),
+            ),
+            SizedBox(
+              height: 10.0,
             ),
             Text(
               "Good Morning",
@@ -42,6 +46,47 @@ class _HomeState extends State<Home> {
                 color: Colors.white,
                 fontWeight: FontWeight.w100,
               ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.circular(20.0),
+                  child: Container(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 150, 235, 221),
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    child: Text(
+                      "Today",
+                      style: TextStyle(
+                          fontSize: 17.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800),
+                    ),
+                  ),
+                ),
+                Text(
+                  "Tomorrow",
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w800),
+                ),
+                Text(
+                  "Next week",
+                  style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w800),
+                ),
+              ],
             ),
           ],
         ),
