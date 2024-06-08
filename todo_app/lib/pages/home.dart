5760d8caa5f3dc820d4c6fa069b6404ed248e780
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-08 21:17:23 +0545

    Creating home page UI

--- todo_app/lib/pages/home.dart
+++ todo_app/lib/pages/home.dart
@@ -1,6 +1,6 @@
-import "package:flutter/cupertino.dart";
+// import "package:flutter/cupertino.dart";
 import "package:flutter/material.dart";
-import "package:flutter/widgets.dart";
+// import "package:flutter/widgets.dart";
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,10 +12,11 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: const Container(
+    return Scaffold(
+      body: Container(
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(gradient: LinearGradient(colors: [Color.fromARGB(255, 12, 75, 192), Color.fromARGB(255, 23, 207, 240), Color.fromARGB(255, 34, 236, 118)],begin: Alignment.topRight, end: Alignment.bottomLeft),),
         child: Column(children: [
 
         ],),
