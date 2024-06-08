143a88f05875c5be208e9d698ea76915a84df706
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-08 21:34:09 +0545

    Creating home page UI

--- todo_app/lib/pages/home.dart
+++ todo_app/lib/pages/home.dart
@@ -14,16 +14,37 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.only(top: 80.0, left: 50.0),
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(gradient: LinearGradient(colors: [Color(0xFF4E65FF), Color(0xFF92EFFD), Color.fromARGB(255, 34, 236, 118)],begin: Alignment.topRight, end: Alignment.bottomLeft),),
-        child: Column(children: [
-          Text("Hello\nNayan", style: TextStyle(
-              fontSize: 25.0,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-          ),)
-        ],),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color(0xFF4E65FF),
+            Color(0xFF92EFFD),
+            Color.fromARGB(255, 34, 236, 118)
+          ], begin: Alignment.topRight, end: Alignment.bottomLeft),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Hello\nNayan",
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              "Good Morning",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
