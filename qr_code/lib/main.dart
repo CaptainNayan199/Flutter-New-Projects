f70e4a1fbde5b2c8fe32001d1618518547eb88b5
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-14 20:03:54 +0545

    Files changed

--- qr_code/lib/main.dart
+++ qr_code/lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         //
         // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
         // and then invoke "hot reload" (save your changes or press the "hot
         // reload" button in a Flutter-supported IDE, or press "r" if you used
         // the command line to start the app).
