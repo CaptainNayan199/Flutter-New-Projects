4e54f46a4ad877e751d780fee014271ef2fa7d39
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-16 20:57:50 +0545

    Made change in the lub file

--- qr_code/lib/main.dart
+++ qr_code/lib/main.dart
@@ -7,20 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        //
-        // and then invoke "hot reloazzd" (save your changes or press the "hot
-        //
-       
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
