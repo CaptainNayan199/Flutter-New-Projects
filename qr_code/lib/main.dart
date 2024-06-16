a622d6bbf673c15cea698cd7f0187101f80381f8
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-16 20:30:33 +0545

    Files updated

--- qr_code/lib/main.dart
+++ qr_code/lib/main.dart
@@ -7,16 +7,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         //
-        // restart instead.
+        // and then invoke "hot reloazzd" (save your changes or press the "hot
         //
-        // This works for code too, not just values: Most code changes can be
-\        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
