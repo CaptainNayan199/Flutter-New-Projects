9dfadb429541ff760c11a6442829db69eaf6c622
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-19 10:38:17 +0545

    New files added

---
+++ qr_code/lib/qr_scanner.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QRScanner extends StatelessWidget {
+  const QRScanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
