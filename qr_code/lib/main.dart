0da457169bd72499c8632b6863b5213cbd673496
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-19 10:44:06 +0545

    Files modifed

--- qr_code/lib/main.dart
+++ qr_code/lib/main.dart
@@ -10,8 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+      ),
       home: QRScanner(),
-      debugShowCheckedModeBanner : false,
+      debugShowCheckedModeBanner: false,
       title: "Scanner",
     );
   }
