3464334cb39ea6b5092602fff12e7939e43a1719
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-24 20:04:48 +0545

    Added come comments in the file

--- qr_code/lib/qr_scanner.dart
+++ qr_code/lib/qr_scanner.dart
@@ -13,7 +13,7 @@ class QRScanner extends StatelessWidget {
         child: Column(
           children: [
             Expanded(child: Container(
-              // The container should be in the center with  some elevation. Container
+              // The container should be in the center with  some elevation. Container should include
 
             ),
             ),
