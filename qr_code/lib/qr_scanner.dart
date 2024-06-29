cd7ec2a95217f3598494cee1f842ffd138044979
captainnayan199 <nayanpathak233199@gmail.com>
2024-06-29 19:31:59 +0545

    Comments removed

--- qr_code/lib/qr_scanner.dart
+++ qr_code/lib/qr_scanner.dart
@@ -13,7 +13,7 @@ class QRScanner extends StatelessWidget {
         child: Column(
           children: [
             Expanded(child: Container(
-              // The container should be in the center with  some elevation. Container should include s small 
+              // The container should be in the center with  some elevation. Container should include 
 
             ),
             ),
