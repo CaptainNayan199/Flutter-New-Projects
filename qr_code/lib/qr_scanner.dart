0f959c96cc1c15ac4bf0e79c30666a61c0e35999
captainnayan199 <nayanpathak233199@gmail.com>
2024-07-01 20:09:04 +0545

    files updated

--- qr_code/lib/qr_scanner.dart
+++ qr_code/lib/qr_scanner.dart
@@ -12,9 +12,20 @@ class QRScanner extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: Column(
           children: [
-            Expanded(child: Container(
-              color: Colors.red,
+            Expanded(
+              child: Container(
+                color: Colors.red,
+              ),
             ),
+            Expanded(
+              child: Container(
+                color: Colors.green,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.amber,
+              ),
             ),
           ],
         ),
