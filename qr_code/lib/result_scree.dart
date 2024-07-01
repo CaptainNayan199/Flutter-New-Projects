2871364bb73ebb726fa3164fa3712bf014bc3d98
captainnayan199 <nayanpathak233199@gmail.com>
2024-07-01 20:45:12 +0545

    result page development and modification

--- qr_code/lib/result_scree.dart
+++ qr_code/lib/result_scree.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code/qr_scanner.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
@@ -6,7 +7,59 @@ class ResultScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        title: Text(
+          "QR Scanner",
+          style: TextStyle(
+              color: Colors.black54,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            Text(
+              "Scanned Result",
+              style: TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Results : ",
+              style: TextStyle(
+                  color: Colors.black87, fontSize: 16, letterSpacing: 1),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width - 100,
+              height: 48,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  
+                ),
+                onPressed: () {},
+                child: Text(
+                  "Copy",
+                  style: TextStyle(
+                      color: Colors.black87, fontSize: 16, letterSpacing: 1),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
